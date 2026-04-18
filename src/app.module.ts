abbaff11d1c06ba4e5a440e1d4d302caf662fa5a
Aryan20apr <aryansingh20nov@gmail.com>
2026-04-19 00:19:58 +0530

    Postgres in docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClsIntegrationModule } from './cls/cls-integration.module';
 import { AppConfigModule } from './config/config.module';
+import { PrismaModule } from './db/prisma.module';
 import { LoggerModule } from './logger/logger.module';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [AppConfigModule, LoggerModule, RedisModule],
+  imports: [
+    AppConfigModule,
+    ClsIntegrationModule,
+    LoggerModule,
+    RedisModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
