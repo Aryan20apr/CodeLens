d3401caf687ab736ffebf5d7dec4854ec78bf60d
Aryan20apr <aryansingh20nov@gmail.com>
2026-05-29 23:17:16 +0530

    Diff module update

--- src/diff/diff.module.ts
+++ src/diff/diff.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 
+import { DiffChunkerService } from './diff-chunker.service';
+import { DiffChunkSerializerService } from './diff-chunk-serializer.service';
 import { DiffParserService } from './diff-parser.service';
 
 @Module({
-  providers: [DiffParserService],
-  exports: [DiffParserService],
+  providers: [
+    DiffParserService,
+    DiffChunkerService,
+    DiffChunkSerializerService,
+  ],
+  exports: [
+    DiffParserService,
+    DiffChunkerService,
+    DiffChunkSerializerService,
+  ],
 })
 export class DiffModule {}
